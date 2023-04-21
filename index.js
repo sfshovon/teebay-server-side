a3b5d4e4710cd6678fc09fc0d294816fa6c1065f
sfshovon <shahriar.shovon@northsouth.edu>
2023-04-21 17:14:53 +0600

    Port Updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const schema = require('./Schemas');
 
 // Initialize the Express app and set the port
 const app = express();
-// const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Apply middleware
 app.use(cors());
@@ -24,6 +24,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server and listen on the specified port
-app.listen(process.env.PORT || 4000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 4000}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
